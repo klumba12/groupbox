9681ffea0d3be889b2fef5835f16ae76b4199ce7
klumba12 <dimitry.sitnov@gmail.com>
2016-05-05 09:06:15 +0300

    setup directive interfaces

--- groupbox.js
+++ groupbox.js
@@ -1,7 +1,37 @@
 (function (angular) {
    'use strict';
 
-   angular.module('groupbox', []);
-   
+   angular.module('groupbox', [])
+       .directive('groupbox', groupboxDirective)
+       .directive('groupboxAll', groupboxAllDirective)
+       .directive('groupboxItem', groupboxItemDirective)
+       .directive('groupboxSelection', groupboxSelectionDirective);
+
+   function groupboxDirective() {
+      return {
+         restrict: 'A'
+      };
+   };
+
+   function groupboxAllDirective() {
+      return {
+         restrict: 'A',
+         require: '^groupbox'
+      };
+   };
+
+   function groupboxItemDirective() {
+      return {
+         restrict: 'A',
+         require: '^groupbox'
+      };
+   };
+
+   function groupboxSelectionDirective() {
+      return {
+         restrict: 'A',
+         require: 'groupbox'
+      };
+   };
 })
 (angular);
